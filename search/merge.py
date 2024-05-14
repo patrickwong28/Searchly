7d6de8205c5c1b818ea50aec6596d4d0905d8de2
FustinCS <FustinCS@gmail.com>
2024-05-13 19:28:50 -0700

    Add merge.py implementation

---
+++ search/merge.py
@@ -0,0 +1,17 @@
+from inverse_index.posting import Posting
+def merge(query1_postings: list[Posting], query2_postings: list[Posting]):
+    result = []
+    query1_index = 0
+    query2_index = 0
+    
+    while query1_index < len(query1_postings) and query2_index < len(query2_postings):
+        if query1_postings[query1_index].docid() == query2_postings[query2_index].docid():
+            result.append(query1_postings[query1_index])
+            query1_index += 1
+            query2_index += 1
+        elif query1_postings[query1_index].docid() < query2_postings[query2_index].docid():
+            query1_index += 1
+        else:
+            query2_index += 1
+    
+    return result
