31338c365ee1d2c478a5dcd69035d7a6f6882707
FustinCS <FustinCS@gmail.com>
2024-05-16 03:58:29 -0700

    Add byte offset mapping function

---
+++ inverse_index/byte_mapping.py
@@ -0,0 +1,15 @@
+def map_byte_offsets(index_file: str):
+    with open(index_file, 'r') as f, open('./inverse_index/index_offsets.txt', 'w+') as output:
+        current_byte_offset = 0
+        while True:
+            line = f.readline()
+            if not line:
+                break
+        
+            token = line.split(' --> ')[0]
+
+            # write to output file
+            output.write(f'{token} - {current_byte_offset}\n')
+            
+            # since in a txt file, every character is a single byte, we can get length of line
+            current_byte_offset += len(line)
