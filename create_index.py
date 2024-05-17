6778f44c1e947e3f15ad2b53ab099d829d1c957d
FustinCS <FustinCS@gmail.com>
2024-05-16 17:41:05 -0700

    Add directory creation for indexes and mapping for organization

--- create_index.py
+++ create_index.py
@@ -3,12 +3,20 @@ from inverse_index.indexer import build_index
 from inverse_index.merge import merge
 from inverse_index.byte_mapping import map_byte_offsets
 import sys
+import os
 
 if __name__ == '__main__':
     try:
         if len(sys.argv) != 2:
             raise IndexError
         file_path = sys.argv[1]
+
+        # create directories to store indexes and mappings
+        if not os.path.exists('./inverse_index/indexes'):
+            os.mkdir('./inverse_index/indexes')
+        if not os.path.exists('./inverse_index/mappings'):
+            os.mkdir('./inverse_index/mappings')
+
         docs = retrieve_documents(file_path)
         build_index(docs)
         merge('./inverse_index/indexes/index_a', './inverse_index/indexes/index_b', 10)
