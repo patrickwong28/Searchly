e73fd3407af1e887eede71c331298ab09045354c
FustinCS <FustinCS@gmail.com>
2024-05-16 21:36:29 -0700

    Add query execution time

--- search/interface.py
+++ search/interface.py
@@ -2,6 +2,7 @@ from search.intersect import merge
 from porter2stemmer import Porter2Stemmer
 from inverse_index.posting import Posting
 from search.parse_mapping import parse_mapping
+import time
 
 def run_interface():
     # load URL mapping and byte offset mapping and store them in dictionaries
@@ -10,6 +11,10 @@ def run_interface():
 
     while True:
         query = input('Query: ').lower()
+
+        # measure execution time
+        start_time = time.time()
+
         query_list = filter_words(query)
 
         try:
@@ -21,15 +26,14 @@ def run_interface():
                     values = line.split(' --> ')[1]
                     fetched_results.append(build_postings(values))
                 fetched_results.sort(key=lambda x: len(x))
+                merged_results = fetched_results[0]
+                for i in range(1, len(fetched_results)):
+                    merged_results = merge(merged_results, fetched_results[i])
         except KeyError:
-            continue
+            merged_results = []
         
-        merged_results = fetched_results[0]
-        for i in range(1, len(fetched_results)):
-            merged_results = merge(merged_results, fetched_results[i])
-       
         print_results(merged_results, url_map)
-
+        print(f'Total query execution time: {int((time.time() - start_time) * 1000)} ms')
         
         
         
@@ -53,5 +57,9 @@ def filter_words(query: str):
     return result
 
 def print_results(results: list[Posting], url_mapping: dict):
-    for result in results:
-        print(url_mapping[str(result.docid)], end='')
+    if len(results) < 5:
+        for result in results:
+            print(url_mapping[str(result.docid)], end='')
+    else:
+        for i in range(5):
+            print(url_mapping[str(results[i].docid)], end='')
