686f730e7be1d9e19af0ce613471ae73082f93eb
FustinCS <FustinCS@gmail.com>
2024-05-21 04:00:51 -0700

    Fix issue with index mapping being different on mac and windows

--- inverse_index/byte_mapping.py
+++ inverse_index/byte_mapping.py
@@ -1,12 +1,12 @@
 def map_byte_offsets(index_file: str):
-    with open(index_file, 'r') as f, open('./inverse_index/mappings/index_offsets.txt', 'w+', encoding='utf-8') as output:
+    with open(index_file, 'rb') as f, open('./inverse_index/mappings/index_offsets.txt', 'w+', encoding='utf-8') as output:
         current_byte_offset = 0
         while True:
             line = f.readline()
             if not line:
                 break
         
-            token = line.split(' --> ')[0]
+            token = line.decode(encoding='utf-8').split(' --> ')[0]
 
             # write to output file
             output.write(f'{token} - {current_byte_offset}\n')
